9d5aa28ea3265bdbefbd51b300546ae0e18b8b4f
tqmvt <66651502+tqmvt@users.noreply.github.com>
2024-05-27 07:23:53 -0700

    feat: update certificate images with nextjs image component

--- components/certificates/Certificates.tsx
+++ components/certificates/Certificates.tsx
@@ -1,4 +1,7 @@
+import Image from "next/image";
+
 import { certificates } from "@/data/content/certificates";
+import { placeholderImagePath } from "@/data/global";
 
 function Designs() {
   return (
@@ -6,10 +9,16 @@ function Designs() {
       {certificates.map((item, index) => {
         return (
           <div className="w-full" key={index}>
-            <a href={item.link} target="_blank" className="w-full">
-              <img
-                className="w-full h-96 hover:opacity-75 transition-opacity object-cover"
+            <a href={item.link} target="_blank" className="w-full relative">
+              <Image
+                className="hover:opacity-75 h-96 object-cover"
                 src={item.img}
+                alt={item.label}
+                width={564}
+                height={564}
+                style={{ width: "100%" }}
+                placeholder="blur"
+                blurDataURL={placeholderImagePath}
               />
             </a>
           </div>
